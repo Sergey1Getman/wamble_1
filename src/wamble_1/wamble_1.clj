3f105a4109f64be4f590553a50a000773032d29d
Sergey1Getman <svchpech1974@gmail.com>
2024-05-13 13:36:59 +0300

    wamble_1

--- src/wamble_1/wamble_1.clj
+++ src/wamble_1/wamble_1.clj
@@ -24,16 +24,21 @@
 (=check (my-reduce + 0 (range 10000)) 49995000)
 
 ;; filter examples
+
 (defn my-filter [pred coll]
   (if (empty? coll)
     coll
     (if (pred (first coll))
       (cons (first coll) (my-filter pred (rest coll)))
       (my-filter pred (rest coll)))))
-
-(=check (my-filter even? [1 2 3 4 5 6]) [2 4 6])
-(=check (my-filter #(> (count %) 3) ["hi" "hello" "hey" "greetings"]) ["hello" "greetings"])
-(=check (my-filter #(and (even? %) (> % 10)) [12 2 13 14 3]) [12 14])
+(defn reduce-filter [f coll] 
+  (reduce (if( f (first coll)
+              (conj coll (first coll))) (rest coll)) coll))
+(defn coll [1 2 3])
+(reduce-filter even? [1 2 3 4 5 6])
+(=check (reduce-filter even? [1 2 3 4 5 6]) [2 4 6])
+(=check (reduce-filter #(> (count %) 3) ["hi" "hello" "hey" "greetings"]) ["hello" "greetings"])
+(=check (reduce-filter #(and (even? %) (> % 10)) [12 2 13 14 3]) [12 14])
 
 ;; concat examples
 (defn my-concat [coll1 coll2]
@@ -52,7 +57,7 @@
 (=check (my-nth [10 20 30 40] 2) 30)
 (=check (my-nth [1 2 3 4] 10) nil) ; Assuming nil for out of bounds
 (=check (my-nth [1 2 3 4] 3) 4)
-
+ 
 ;; max/min examples
 (defn my-max [coll]
   (if (empty? coll)
@@ -95,68 +100,46 @@
 
 (=check (my-take 3 [5 4 3 2 1]) [5 4 3])
 
+(defn my-map[])
+(defn sum [result coll]
+  
+    (if (seq coll)
+      (sum (+ result (first coll))
+           (rest coll))
+      result))
+(sum 0 [1 2 3])
 ;; merge examples
+(merge {} {})
 (defn my-merge [map1 map2]
   (if (empty? map1)
     map2
-    (let [[key value] (first map2)
-          merged-map (assoc map1 key value)]
-      (recur merged-map (rest map2)))))
+    (reduce (fn [result-map [key value]]
+              (assoc result-map key value)) map2 map1)))
 (=check (my-merge {:a 1 :b 2} {:b 3 :c 4}) {:a 1 :b 3 :c 4})
 (=check (my-merge {:foo "bar"} {:foo "baz", :hello "world"}) {:foo "baz", :hello "world"})
 (=check (my-merge {} {:a 1}) {:a 1})
 
 ;; group-by examples
-(defn my-group-by [f coll]
-  (letfn [(group-helper [coll result]
-            (if (empty? coll)
-              result
-              (let [current (first coll)
-                    key (f current)
-                    grouped (get result key [])]
-                (recur (rest coll) (assoc result key (conj grouped current))))))]
-    (group-helper coll {})))
+(defn my-group-by [f coll])
 (=check (my-group-by :type [{:type :a :value 1} {:type :b :value 2} {:type :a :value 3}])
         {:a [{:type :a :value 1} {:type :a :value 3}], :b [{:type :b :value 2}]})
 (=check (my-group-by even? [1 2 3 4 5 6]) {true [2 4 6], false [1 3 5]})
 (=check (my-group-by count ["one" "two" "three" "four"]) {3 ["one" "two"], 5 ["three"], 4 ["four"]})
 
 ;; keys examples
-(defn my-keys [map]
-  (letfn [(keys-helper [remaining-keys result]
-            (if (empty? remaining-keys)
-              result
-              (let [current-key (first remaining-keys)]
-                (recur (rest remaining-keys) (conj result current-key)))))]
-    (keys-helper (keys map) [])))
+(defn my-keys [map])
 (=check (my-keys {:a 1 :b 2 :c 3}) [:a :b :c])
 (=check (my-keys {:foo "bar" :baz "qux"}) [:foo :baz])
 (=check (my-keys {}) [])
 
 ;; vals examples
-(defn my-vals [map]
-  (letfn [(vals-helper [remaining-keys result]
-            (if (empty? remaining-keys)
-              result
-              (let [current-key (first remaining-keys)]
-                (recur (rest remaining-keys) (conj result (get map current-key))))))]
-    (vals-helper (keys map) [])))
+(defn my-vals [map])
 (=check (my-vals {:a 1 :b 2 :c 3}) [1 2 3])
 (=check (my-vals {:foo "bar" :baz "qux"}) ["bar" "qux"])
 (=check (my-vals {}) [])
 
 ;; select-keys examples
-(defn my-select-keys [map keys]
-  (loop [remaining-keys keys
-         result {}]
-    (if (empty? remaining-keys)
-      result
-      (let [current-key (first remaining-keys)
-            current-value (get map current-key)]
-        (recur (rest remaining-keys)
-               (if current-value
-                 (assoc result current-key current-value)
-                 result))))))
+(defn my-select-keys [map keys])
 (=check (my-select-keys {:a 1 :b 2 :c 3} [:a :c]) {:a 1 :c 3})
 (=check (my-select-keys {:name "Alice" :age 30 :gender "Female"} [:name :age]) {:name "Alice", :age 30})
 (=check (my-select-keys {:foo "bar" :hello "world"} [:foo]) {:foo "bar"})
