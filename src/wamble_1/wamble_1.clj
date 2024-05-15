bcbf001ebd2ceb275c908d4dfb2f079957c66aa1
Sergey1Getman <svchpech1974@gmail.com>
2024-05-16 01:00:55 +0300

    wamble_1

--- src/wamble_1/wamble_1.clj
+++ src/wamble_1/wamble_1.clj
@@ -24,62 +24,40 @@
 (=check (my-reduce + 0 (range 10000)) 49995000)
 
 ;; filter examples
-
-(defn my-filter [pred coll]
-  (if (empty? coll)
-    coll
-    (if (pred (first coll))
-      (cons (first coll) (my-filter pred (rest coll)))
-      (my-filter pred (rest coll)))))
 (defn reduce-filter [f coll] 
-  (reduce (if( f (first coll)
-              (conj coll (first coll))) (rest coll)) coll))
-(defn coll [1 2 3])
+  (reduce (fn [result item]
+            (if (f item)
+              (conj result item)
+              result)) [] coll))
 (reduce-filter even? [1 2 3 4 5 6])
 (=check (reduce-filter even? [1 2 3 4 5 6]) [2 4 6])
 (=check (reduce-filter #(> (count %) 3) ["hi" "hello" "hey" "greetings"]) ["hello" "greetings"])
 (=check (reduce-filter #(and (even? %) (> % 10)) [12 2 13 14 3]) [12 14])
 
 ;; concat examples
-(defn my-concat [coll1 coll2]
-  (if (empty? coll2)
-    coll1
-    (recur (conj coll1 (first coll2)) (rest coll2))))
-(my-concat [1 2] [3 4])
-(=check (my-concat [1 2] [3 4]) [1 2 3 4])
-(=check (count (my-concat (range 5000) (range 5000 10000))) 10000)
+(defn reduce-concat [coll1 coll2]
+  (reduce conj coll1 coll2)) 
+(reduce-concat [1 2] [3 4])
+(=check (reduce-concat[1 2] [3 4]) [1 2 3 4])
+(=check (count (reduce-concat (range 5000) (range 5000 10000))) 10000)
 
 ;; nth examples
-(defn my-nth [coll index]
-  (if (= index 0)
-    (first coll)
-    (my-nth (rest coll) (- index 1))))
-(=check (my-nth [10 20 30 40] 2) 30)
-(=check (my-nth [1 2 3 4] 10) nil) ; Assuming nil for out of bounds
-(=check (my-nth [1 2 3 4] 3) 4)
- 
+(defn reduce-nth [coll index]);;???????
+(=check (reduce-nth [10 20 30 40] 2) 30)
+(=check (reduce-nth [1 2 3 4] 10) nil) ; Assuming nil for out of bounds
+(=check (reduce-nth [1 2 3 4] 3) 4)
 ;; max/min examples
-(defn my-max [coll]
-  (if (empty? coll)
-    nil
-    (let [max-rest (my-max (rest coll))]
-      (if (or (nil? max-rest) (> (first coll) max-rest))
-        (first coll)
-        max-rest))))
-  
-(defn my-min [coll]
-  (if (empty? coll)
-    nil
-    (let [min-rest (my-min (rest coll))]
-      (if (or (nil? min-rest) (< (first coll) min-rest))
-        (first coll)
-        min-rest))))
-(=check (my-max [5 3 9 1]) 9)
-(=check (my-min [5 3 9 1]) 1)
-(=check (my-max [-5 -3 -9 -1]) -1)
-(=check (my-min [-5 -3 -9 -1]) -9)
-(=check (my-max []) nil)
-(=check (my-min []) nil)
+  (defn reduce_max [coll]
+    (reduce (fn [x y] 
+              (if (> x y) x y)) coll))
+(defn reduce_min [coll])
+
+(=check (reduce_max [5 3 9 1]) 9)
+(=check (reduce_min[5 3 9 1]) 1)
+(=check (reduce_max [-5 -3 -9 -1]) -1)
+(=check (reduce_min [-5 -3 -9 -1]) -9)
+(=check (reduce_min []) nil)
+(=check (reduce_max []) nil)
 
 ;; count examples
 (defn my-count [coll]
@@ -87,7 +65,7 @@
     0
     (let [index (my-count (rest coll))]
     (+ index 1)  )))
-(=check (my-count [1 2 3 4 5]) 5)
+(=check (reduce-count [1 2 3 4 5]) 5)
 (=check (my-count [[1 2] [3 4] [5]]) 3)
 (=check (my-count []) 0)
 
@@ -97,49 +75,54 @@
     coll
     (if (pos? n)
       (cons (first coll) (my-take (dec n) (rest coll))))))
-
+(defn reduce-take [n coll])
 (=check (my-take 3 [5 4 3 2 1]) [5 4 3])
 
-(defn my-map[])
-(defn sum [result coll]
-  
-    (if (seq coll)
-      (sum (+ result (first coll))
-           (rest coll))
-      result))
-(sum 0 [1 2 3])
+
 ;; merge examples
-(merge {} {})
 (defn my-merge [map1 map2]
   (if (empty? map1)
     map2
     (reduce (fn [result-map [key value]]
-              (assoc result-map key value)) map2 map1)))
+              (assoc result-map key value)) map1 map2)))
 (=check (my-merge {:a 1 :b 2} {:b 3 :c 4}) {:a 1 :b 3 :c 4})
 (=check (my-merge {:foo "bar"} {:foo "baz", :hello "world"}) {:foo "baz", :hello "world"})
 (=check (my-merge {} {:a 1}) {:a 1})
+group-by
 
 ;; group-by examples
-(defn my-group-by [f coll])
+(defn my-group-by [f coll]
+  (reduce (fn [acc elem]
+            (let [k (f elem)] 
+              (assoc acc k (conj (get acc k []) elem))))
+          {}
+          coll))
 (=check (my-group-by :type [{:type :a :value 1} {:type :b :value 2} {:type :a :value 3}])
         {:a [{:type :a :value 1} {:type :a :value 3}], :b [{:type :b :value 2}]})
 (=check (my-group-by even? [1 2 3 4 5 6]) {true [2 4 6], false [1 3 5]})
 (=check (my-group-by count ["one" "two" "three" "four"]) {3 ["one" "two"], 5 ["three"], 4 ["four"]})
 
 ;; keys examples
-(defn my-keys [map])
+
+(defn my-keys [map]
+  (reduce (fn [result-map [keys]]
+            (conj result-map keys)) [] map))
+(my-keys {:a 1 :b 2 :c 3})
 (=check (my-keys {:a 1 :b 2 :c 3}) [:a :b :c])
 (=check (my-keys {:foo "bar" :baz "qux"}) [:foo :baz])
 (=check (my-keys {}) [])
-
 ;; vals examples
-(defn my-vals [map])
+(defn my-vals [map]
+(reduce (fn [result-map [values]]
+          (conj result-map values))  [] map))
 (=check (my-vals {:a 1 :b 2 :c 3}) [1 2 3])
 (=check (my-vals {:foo "bar" :baz "qux"}) ["bar" "qux"])
 (=check (my-vals {}) [])
 
 ;; select-keys examples
-(defn my-select-keys [map keys])
+
+(defn my-select-keys [map keys];;?????????
+  (reduce (fn [ret keyseq]) map keys))
 (=check (my-select-keys {:a 1 :b 2 :c 3} [:a :c]) {:a 1 :c 3})
 (=check (my-select-keys {:name "Alice" :age 30 :gender "Female"} [:name :age]) {:name "Alice", :age 30})
 (=check (my-select-keys {:foo "bar" :hello "world"} [:foo]) {:foo "bar"})
---
+++ src/wamble_1/wamble_2.clj
@@ -0,0 +1,24 @@
+(ns wamble_2.wamle_2)
+(def result (atom {:correct 0 :incorrect 0}))
+(defmacro =check [left right]
+  `(let [left# ~left
+         right# ~right]
+     (if (= right# left#)
+       (swap! result update :correct inc)
+       (do
+         (println "missmatch:" ~(str (first left)) left# right#)
+         (swap! result update :incorrect inc)))
+     left#))
+(defn my-merge-with
+  [f & maps]
+  (reduce (fn [acc m]
+            (reduce (fn [result [k v]]
+                      (update result k f v))
+                    acc
+                    m))
+          (first maps)
+          (rest maps)))
+(=check (my-merge-with * {:a 2, :b 3, :c 4} {:a 2} {:b 2} {:c 5})
+        {:a 4, :b 6, :c 20})
+(=check (my-merge-with concat {:a [3], :b [6]} {:a [4 5], :c [8 9]} {:b [7]}) 
+        {:a [3 4 5], :b [6 7], :c [8 9]})
